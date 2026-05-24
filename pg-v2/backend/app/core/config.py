43ec0fe76a04a7dae04ad045257cf160677255d6
Jolonto <aluyapeter7@gmail.com>
2026-05-24 09:15:37 +0100

    minor changes for temporary deployment

--- pg-v2/backend/app/core/config.py
+++ pg-v2/backend/app/core/config.py
@@ -15,6 +15,8 @@ class Settings(BaseSettings):
     JWT_SECRET_KEY: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 7
 
+    FRONTEND_URL: str | None = None
+
     model_config = SettingsConfigDict(
         env_file=".env",
         env_file_encoding="utf-8",
